d62f190a09b6354a0baedb767e2dba3c10d15993
Pasupuleti-Meghana <144216972+Pasupuleti-Meghana@users.noreply.github.com>
2026-01-25 23:26:37 +0530

    Time: 25 ms (13.21%) | Memory: 5.4 MB (76.92%) - LeetSync

---
+++ 1-two-sum/two-sum.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+    length := len(nums)
+    for i:=0;i<length-1;i++ {
+        for j:=i+1;j<length;j++ {
+            if nums[i]+nums[j] == target {
+                return []int{i,j}
+            }
+        }
+    }
+    return []int{}
+}
