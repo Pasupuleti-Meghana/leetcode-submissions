15032998b1f65828ff56c78ab222d148695fad1f
Pasupuleti-Meghana <144216972+Pasupuleti-Meghana@users.noreply.github.com>
2026-05-09 12:31:16 +0530

    Time: 3 ms (38.66%) | Memory: 6.1 MB (13.51%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,11 +1,11 @@
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
-    for i,num := range nums{
-        needed := target - num
-        if ind,found := m[needed]; found {
+    for i, num := range nums {
+        required := target - num
+        if ind, found := m[required]; found{
             return []int{ind,i}
         }
         m[num]=i
     }
-    return []int{}
+    return nil
 }
