ecb758518b2448f4676151c440bbea1077d88444
Pasupuleti-Meghana <144216972+Pasupuleti-Meghana@users.noreply.github.com>
2026-07-03 10:33:16 +0530

    Time: 0 ms (100.00%) | Memory: 4.5 MB (1.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.go
@@ -10,13 +10,13 @@ func search(nums []int, target int) int {
         }
 
         if nums[left] <= nums[mid] {
-            if nums[left] <= target && target <= nums[mid] {
+            if nums[left] <= target && target < nums[mid] {
                 right = mid - 1
             } else {
                 left = mid + 1
             }
         } else {
-            if nums[mid] <= target && target <= nums[right] {
+            if nums[mid] < target && target <= nums[right] {
                 left = mid + 1
             } else {
                 right = mid - 1
