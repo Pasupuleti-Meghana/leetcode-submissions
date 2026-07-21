f53d2e7f460843ef60500e12882e06729ab03737
Pasupuleti-Meghana <144216972+Pasupuleti-Meghana@users.noreply.github.com>
2026-07-21 19:22:35 +0530

    Time: 0 ms (100.00%) | Memory: 4.9 MB (76.54%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.go
@@ -0,0 +1,19 @@
+func findMin(nums []int) int {
+    n := len(nums)
+    left := 0
+    right := n-1
+
+    for left <= right {
+        mid := (left + right) / 2
+
+        if nums[mid] > nums[right] {
+            left = mid + 1
+        } else if nums [mid] < nums[right] {
+            right = mid
+        } else {
+            right--
+        }
+    }
+
+    return nums[left]
+}
